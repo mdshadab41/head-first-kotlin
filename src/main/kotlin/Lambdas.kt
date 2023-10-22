f1258456f4562cefaea0e4fc481f9fb880d4e092
mdshadab41 <shadabjhu00447@gmail.com>
2023-10-22 13:09:13 +0530

    Commit - 2: Lambdas

--- src/main/kotlin/Lambdas.kt
+++ src/main/kotlin/Lambdas.kt
@@ -1,17 +1,33 @@
 fun main(){
-    var addFive = {x: Int -> x + 5}
-    println("Pass 6 to addFive: ${addFive(6)}")
+//    var addFive = {x: Int -> x + 5}
+//    println("Pass 6 to addFive: ${addFive(6)}")
+//
+//    val addInts = {x: Int, y: Int -> x+y}
+//    val result = addInts.invoke(6,7)
+//    println(result)
+//
+//    val inLambdas: (Int, Int) -> Int = {x,y -> x +y}
+//    println(inLambdas(7,8))
+//
+//    val addSeven: (Int) -> Int = {it +7}
+//    println(addSeven(7))
+//
+//    val myLambdas: () -> Unit = { println("Hi Lambdas") }
+//    myLambdas()
 
-    val addInts = {x: Int, y: Int -> x+y}
-    val result = addInts.invoke(6,7)
-    println(result)
+    fun convert(x: Double,
+               converter: (Double) -> Double): Double {
 
-    val inLambdas: (Int, Int) -> Int = {x,y -> x +y}
-    println(inLambdas(7,8))
+        val result = converter(x)
+        println(result)
+        return result
+    }
+    fun convertFive(converter: (Int) -> Double) : Double {
+        val result = converter(5)
+        println(result)
+        return result
+    }
+    convert(20.0){it * 1.8 + 32}
+    convertFive { it * 1.8 +32  }
 
-    val addSeven: (Int) -> Int = {it +7}
-    println(addSeven(7))
-
-    val myLambdas: () -> Unit = { println("Hi Lambdas") }
-    myLambdas()
 }
